0bf2f7f4f6bb63e9b3566ed313e163ac8e1159a5
JelaJs <nikola4434@gmail.com>
2025-02-07 13:44:48 +0100

    added custom navbar to profile page, dashboard blade deleted

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -10,7 +10,7 @@
 <body class="h-full">
 <div class="min-h-full">
     
-    @include('layout/nav-bar')
+    @include('layout.nav-bar')
   
     <header class="bg-white shadow-sm">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
 
 ////////////////////////////////
 //AUTH
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
