2f0577199b836ad7cb410b5e38fb876bc6b2f7f0
JelaJs <nikola4434@gmail.com>
2025-02-07 12:03:16 +0100

    auth installed, added role to register form

---
+++ resources/views/components/form-select.blade.php
@@ -0,0 +1,4 @@
+
+<select {{$attributes}} class="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700">
+    {{$slot}}
+</select>
