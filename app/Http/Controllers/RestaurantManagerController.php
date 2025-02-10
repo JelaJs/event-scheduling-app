fdf42624492f31adf5aa5b6afc2f957de1f62ad5
JelaJs <nikola4434@gmail.com>
2025-02-10 14:40:31 +0100

    EditRestaurant blade and RestaurantManagerController refactored

--- app/Http/Controllers/RestaurantManagerController.php
+++ app/Http/Controllers/RestaurantManagerController.php
@@ -26,25 +26,12 @@ class RestaurantManagerController extends Controller
 
     public function store(RestorauntStoreRequest $request) {
 
-        if(Restaurants::firstWhere('user_id', Auth::id())) {
-            return redirect()->route('home');
-        }
+        if(Restaurants::firstWhere('user_id', Auth::id())) return redirect()->route('home');
 
-        $bcgPath = null;
-        if($request->hasFile('background_image')) {
-
-            $bcgPath = $request->file('background_image')->store('uploads', 'public');
-        }
+        $bcgPath = $this->restaurantRepo->checkAndAssignBcgPath($request);
 
         $imagePaths = [];
-        if ($request->hasFile('images')) {
-
-            foreach ($request->file('images') as $image) {
-
-                $path = $image->store('uploads', 'public');
-                $imagePaths[] = $path;
-            }
-        }
+        $this->restaurantRepo->checkAndAssingImgPaths($request, $imagePaths);
 
         $this->restaurantRepo->store($request, $bcgPath, $imagePaths);
 
@@ -53,10 +40,7 @@ class RestaurantManagerController extends Controller
 
     public function edit(Restaurants $restaurant) {
 
-        if($restaurant->user_id !== Auth::id()) {
-
-            return redirect()->back();
-        }
+        if($restaurant->user_id !== Auth::id()) return redirect()->back();
 
         return view('manager.editRestaurant', [
             'restaurant' => $restaurant
@@ -65,29 +49,13 @@ class RestaurantManagerController extends Controller
 
     public function update(RestorauntStoreRequest $request, Restaurants $restaurant) {
 
-        if($restaurant->user_id !== Auth::id()) {
+        if($restaurant->user_id !== Auth::id()) return redirect()->back();
 
-            return redirect()->back();
-        }
+        $this->restaurantRepo->checkAndUpdateRestaurantBcg($request, $restaurant);
 
-        if($request->hasFile('background_image')) {
+        $this->restaurantRepo->checkAndUpdateImgPaths($request, $restaurant);
 
-            $restaurant->background_image = $request->file('background_image')->store('uploads', 'public');
-        }
-
-        if ($request->hasFile('images')) {
-            $imagePaths = array_map(fn($image) => $image->store('uploads', 'public'), $request->file('images'));
-    
-            $restaurant->image_1 = $imagePaths[0] ?? $restaurant->image_1;
-            $restaurant->image_2 = $imagePaths[1] ?? $restaurant->image_2;
-            $restaurant->image_3 = $imagePaths[2] ?? $restaurant->image_3;
-        }
-
-        $restaurant->fill($request->only([
-            'name', 'description', 'instagram', 'youtube', 'phone_number'
-        ]));
-
-        $restaurant->save();
+        $this->restaurantRepo->fillAndSave($request, $restaurant);
 
         return redirect()->route('manager.restaurant.index');
     }
--- app/Repositories/RestaurantManagerRepository.php
+++ app/Repositories/RestaurantManagerRepository.php
@@ -28,5 +28,57 @@ class RestaurantManagerRepository {
             'youtube' => $request->youtube,
             'phone_number' => $request->phone_number
         ]);
+
+        
+    }
+
+    public function checkAndAssignBcgPath($request) {
+
+        if($request->hasFile('background_image')) {
+
+            return $request->file('background_image')->store('uploads', 'public');
+        }
+
+        return null;
+    }
+
+    public function checkAndAssingImgPaths($request, &$imagePaths) {
+
+        if ($request->hasFile('images')) {
+
+            foreach ($request->file('images') as $image) {
+
+                $path = $image->store('uploads', 'public');
+                $imagePaths[] = $path;
+            }
+        }
+    }
+
+    public function checkAndUpdateRestaurantBcg($request, $restaurant) {
+
+        if($request->hasFile('background_image')) {
+
+            $restaurant->background_image = $request->file('background_image')->store('uploads', 'public');
+        }
+    }
+
+    public function checkAndUpdateImgPaths($request, $restaurant) {
+
+        if ($request->hasFile('images')) {
+            $imagePaths = array_map(fn($image) => $image->store('uploads', 'public'), $request->file('images'));
+    
+            $restaurant->image_1 = $imagePaths[0] ?? $restaurant->image_1;
+            $restaurant->image_2 = $imagePaths[1] ?? $restaurant->image_2;
+            $restaurant->image_3 = $imagePaths[2] ?? $restaurant->image_3;
+        }
+    }
+
+    public function fillAndSave($request, $restaurant) {
+
+        $restaurant->fill($request->only([
+            'name', 'description', 'instagram', 'youtube', 'phone_number'
+        ]));
+
+        $restaurant->save();
     }
 }
