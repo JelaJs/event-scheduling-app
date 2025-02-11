a5270e8d50b40d500ee262f6ad50497dfcc37bc4
JelaJs <nikola4434@gmail.com>
2025-02-11 14:09:12 +0100

    added TABLE as const

--- database/migrations/2025_02_10_165634_add_adress_to_restaurant.php
+++ database/migrations/2025_02_10_165634_add_adress_to_restaurant.php
@@ -1,24 +1,20 @@
 <?php
 
+use App\Models\Restaurants;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::table('restaurants', function (Blueprint $table) {
+        Schema::table(Restaurants::TABLE, function (Blueprint $table) {
             $table->string('address');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::table('restaurant', function (Blueprint $table) {
