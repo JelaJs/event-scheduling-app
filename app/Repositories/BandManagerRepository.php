f2ead6e9eea82fa5d6a7adb2ef9d05874bc43e78
JelaJs <nikola4434@gmail.com>
2025-02-14 17:37:36 +0100

    Store Band refactored

--- app/Repositories/BandManagerRepository.php
+++ app/Repositories/BandManagerRepository.php
@@ -2,33 +2,42 @@
 
 namespace App\Repositories;
 
+use App\Models\BandImages;
 use App\Models\Bands;
 use Illuminate\Support\Facades\Auth;
 
 class BandManagerRepository extends RestaurantManagerRepository {
 
     private $bandModel;
+    private $bandImagesModel;
     public function __construct() {
 
         parent::__construct();
         $this->bandModel = new Bands();
+        $this->bandImagesModel = new BandImages();
     }
 
     public function store($request, $bcgPath, $imagePaths) {
 
-        $this->bandModel->create([
+        $band = $this->bandModel->create([
 
             'user_id' => Auth::id(), 
             'name' => $request->name,
             'background_image' => $bcgPath,
-            'image_1' => $imagePaths[0] ?? null,
-            'image_2' => $imagePaths[1] ?? null,
-            'image_3' => $imagePaths[3] ?? null,
             'description' => $request->description,
             'instagram' => $request->instagram,
             'youtube' => $request->youtube,
             'phone_number' => $request->phone_number,
         ]);
+
+        foreach($imagePaths as $imgPath) {
+            
+            $this->bandImagesModel->create([
+                'bands_id' => $band->id,
+                'image' => $imgPath,
+                'user_id' => Auth::id()
+            ]);
+        }
     }
 
     public function fillAndSaveBand($request, $band) {
