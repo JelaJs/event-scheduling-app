1b69292e257de2677aafd9cccf87510d6aa223bf
JelaJs <nikola4434@gmail.com>
2025-02-27 00:23:15 +0100

    code refactoring, comment inside Helper\Status

--- app/Helper/Status.php
+++ app/Helper/Status.php
@@ -6,6 +6,8 @@ use App\Models\Reservations;
 
 class Status {
 
+    //Napomenuo si mi da je ova Helper funckija nepotrebna (kolko sam skontao na nju si mislio jer je jedina)
+    //Sa njom ispisujem Constante unutar blade, prilikom if provera itd... Cini mi se da je ovako citljivije unutar blade pa bih ostavio
     public function pending(): string
     {
         return Reservations::PENDING_STATUS;
